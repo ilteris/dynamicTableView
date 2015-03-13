071b36963f06cbb87a3d9ec1d54d23df3a6c436e
ilteris <ilteris@gmail.com>
2015-03-13 15:23:47 -0400

    Initial Commit

---
+++ KeepTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeepTest
+//
+//  Created by ilteris on 3/13/15.
+//  Copyright (c) 2015 ilteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeepTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeepTest
+//
+//  Created by ilteris on 3/13/15.
+//  Copyright (c) 2015 ilteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
